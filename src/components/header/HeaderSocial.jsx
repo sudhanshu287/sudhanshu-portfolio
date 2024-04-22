72ede95f2092935b404230116917e3816f5c9683
Sudhanshu Patel <patelsudhanshu290@gmail.com>
2024-04-22 18:16:38 +0530

    in headers section mail socials icon is added

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs';
-import {BsGithub} from 'react-icons/bs';
-import {BsInstagram} from 'react-icons/bs';
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { MdAttachEmail } from "react-icons/md";
 const HeaderSocial = () => {
   return (
     <div className="header__socials">
-        <a href="https://linkedin.com/in/sudhanshu287" target="_blank"><BsLinkedin /></a>
-        <a href="https://github.com/sudhanshu287" target="_blank"><BsGithub/></a>
-        <a href="https://instagram.com/sudhanshu_287" target="_blank"><BsInstagram/></a>
+      <a href="mailto:sdn.patel.287@gmail.com" target="_blank">
+      <MdAttachEmail />
+      </a>
+      <a href="https://linkedin.com/in/sudhanshu287" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/sudhanshu287" target="_blank">
+        <BsGithub />
+      </a>
+      <a href="https://instagram.com/sudhanshu_287" target="_blank">
+        <BsInstagram />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocial
+export default HeaderSocial;
